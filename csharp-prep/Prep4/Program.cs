721e65270e158c30d08f8cb85649f00dca7e1973
joshua.burkhan <bai21030@byui.edu>
2024-09-22 06:16:55 -0500

    prep 4 and 5, week01

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        //The sum list
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of your numbers is: {sum}");
+
+        //the Average number
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average of your numbers is: {average:F2}");
+
+        //the max number:
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max of your numbers is: {max}");
+
     }
 }
