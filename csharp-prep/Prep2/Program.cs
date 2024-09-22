0bca8455a9f82544a50df09f5d44b0d7aa9156da
joshua.burkhan <bai21030@byui.edu>
2024-09-22 05:47:47 -0500

    3 preps ready, week01

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Tell me the % of your Grade: ");
+        string answare = Console.ReadLine();
+        int grade = int.Parse(answare);
+
+        string letter = "";
+        string exact_grade = "";
+
+        int A = 90;
+        int B = 80;
+        int C = 70;
+        int D = 60;
+
+        if (grade >= A)
+        {
+            letter = "A";
+        }
+        else if (grade >= B)
+        {
+            letter = "B";
+        }
+        else if (grade >= C)
+        {
+            letter = "C";
+        }
+        else if (grade >= D)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade % 10 >= 7 && grade % 10 <=9) 
+        {
+            exact_grade = "+";
+        }
+        else if (grade % 10 >= 0 && grade % 10 <3)
+        {
+            exact_grade = "-";
+        } 
+        else
+        {
+            exact_grade = "";
+        }
+
+        Console.WriteLine($"Your Grade is: {letter}{exact_grade}");
+
+        if (grade >= C) 
+        {
+            Console.Write("Great! You passed the course!");
+        }
+
+        else 
+        {
+            Console.Write("You didn't pass the course, but don't be discouraged, be prepared and you'll do better next time!");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ")
+        //int number = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("I'm thinking of a magic number, try to guess it :)");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guess_number = 0;
+
+        while (guess_number != number)
+        {
+            Console.Write("What is your guess? ");
+            guess_number = int.Parse(Console.ReadLine());
+
+            if (guess_number < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess_number > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        }
+
+        Console.WriteLine("Nice, you guessed it!");
     }
 }
