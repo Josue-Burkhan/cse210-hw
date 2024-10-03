38b13441ed5a1bed9e740120e6a7766dec92aa8b
joshua.burkhan <bai21030@byui.edu>
2024-10-03 16:09:34 -0500

    Scripture memories program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,24 @@
 using System;
 
+class Scripture 
+{
+    public string MyScripture()
+    {
+        string Myvers = "But behold, all things have been done in the wisdom of him who knoweth all things. Adam fell that men might be; and men are, that they might have joy."; 
+        return Myvers;
+    }
+    public string TitleMyVers()
+    {
+        string titleVers = "2 Nephie 2:24-25: ";
+        return titleVers;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Changes changesWords = new Changes();
+        changesWords.conditional();
     }
 }
