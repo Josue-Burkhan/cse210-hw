e003bd0a780e6c39bd27801b2c5852d9e5318285
joshua.burkhan <bai21030@byui.edu>
2024-10-12 07:43:26 -0500

    last

--- foundation/Foundation1/Comment.cs
+++ foundation/Foundation1/Comment.cs
@@ -1,10 +1,8 @@
 public class Comment
 {
-    // Propiedades para el nombre del usuario y el texto del comentario.
     public string CommenterName { get; set; }
     public string CommentText { get; set; }
 
-    // Constructor para inicializar el comentario con nombre y texto.
     public Comment(string commenterName, string commentText)
     {
         CommenterName = commenterName;
